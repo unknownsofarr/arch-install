9b5566612169c9390d67bd3db33f3dca29a634b8
Hrushikesh Dolas <39385750+hdivine@users.noreply.github.com>
2021-09-18 19:55:11 +0530

    fixed bugs

--- base.sh
+++ base.sh
@@ -1,5 +1,5 @@
 # timezone setup
-timedatectl set-ntp true
+timedatectl set-ntp true &>/dev/null
 while [[ 1 ]]; do
 	clear
 	echo -e '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
@@ -31,68 +31,71 @@ while [[ 1 ]]; do
 	fi
 	break
 done
-ln -sf $BASE_FILE_PATH'/'$ZONE'/'$TIMEZONE /etc/localtime
-hwclock --systohc
+ln -sf $BASE_FILE_PATH'/'$ZONE'/'$TIMEZONE /etc/localtime &>/dev/null
+hwclock --systohc &>/dev/null
 clear
-echo '[^_^] Timezone setup complete...'
+echo '[o_o] Timezone setup complete...'
 
 # setting up local
-echo '[^_^] Settingup locale...'
+echo -e '\n[o_o] Settingup locale...'
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'LANG="en_US.UTF-8"' > /etc/locale.conf
-locale-gen
-sleep 2
+locale-gen &>/dev/null
 
 # setting up hotnames
-echo '[o_o] Settingup hostname...'
+echo -e '\n[o_o] Settingup hostname...'
 read -p '[o_o] Enter hostname for your machine -> ' HNAME
 echo $HNAME > /etc/hostname
 echo -e "127.0.0.1		localhost\n::1		localhost\n127.0.1.1	$HNAME.localdomain $HNAME" > /etc/hosts
-echo '[o_o] Host setup complete...'
+echo -e '[o_o] Host setup complete...\n'
 
 # users and passwords
-read -p '[o_o] Would you like to setup root user(y/n) -> ' RUSER
+read -p '[o_o] Would you like to setup root user. (y/n) -> ' RUSER
 if [ $RUSER == 'y' ]; then
 	passwd
 fi
 
-read -p '[o_o] Would you like to add user(y/n) ->' NEWUSR
+read -p '[o_o] Would you like to add user. (y/n) ->' NEWUSR
 while [[ $NEWUSR == 'y' ]]; do
 	read -p '[o_o] Enter username -> ' USERNAME
 	useradd -m $USERNAME
 	passwd $USERNAME
 	echo '[o_o] $USERNAME is created...adding it to wheel, audio, video, storage, optical group'
 	usermod -aG wheel,audio,video,storage,optical $USERNAME
-
-	read -p '[o_o] Would you like to add another user(y/n) ->' NEWUSR
+	echo -e '\n'
+	read -p '[o_o] Would you like to add another user. (y/n) ->' NEWUSR
 done
 
 # seting up sudo 
-echo '[o_o] Installing sudo...'
+echo '\n[o_o] Installing sudo...'
 pacman -S sudo nano
+clear
 
 REPEAT='y'
 while [[ $REPEAT == 'y' ]]; do
 	clear
 	echo '[o_o] In the next step nano editor will openup. If you did not understand press "Control+x" to exit. You can do this agin if required'
 	echo '[o_o] You are suppouse to uncomment this line.'
-	echo -e '\n%wheel ALL=(ALL) ALL\n'
-	read -p 'type "ok" to procced' OK
+	echo -e '\n%wheel ALL=(ALL) ALL\n\n'
+	read -p '[o_o] Type "ok" to procced -> ' OK
 
 	if [[ $OK == 'ok' ]]; then
 		EDITOR=nano visudo
 	fi
-	
+	echo -e '------------------------------------------------------------------------\n'
 	read -p "[o_o] If you have made any mistek type 'y' to repeat...or press enter to continue..." REPEAT
 done
 
 clear
 echo "[o_o] Installing grub..."
 pacman -S grub efibootmgr dosfstools os-prober mtools
+clear
 
 mkdir /boot/EFI
-mount $BOOTPART /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+lsblk
+read -p '[o_o] Enter EFI partition (/dev/sdXX) -> ' EFIPART
+mount $EFIPART /boot/EFI 
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck 
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo '[o_o] Installing and seting up NetworkManager'
@@ -103,7 +106,3 @@ echo '[*_o] Arch Linux is installed type these commands'
 echo 'exit'
 echo 'umount -l /mnt'
 echo 'reboot'
-
-
-
-
